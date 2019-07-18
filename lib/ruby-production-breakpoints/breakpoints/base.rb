e30944a9cbe79a2a92504c66309ed87b2e4f3baf
Dale Hamel <dale.hamel@srvthe.net>
2019-07-18 18:37:42 -0400

    ELF parsing tests

--- lib/ruby-production-breakpoints/breakpoints/base.rb
+++ lib/ruby-production-breakpoints/breakpoints/base.rb
@@ -4,6 +4,7 @@ using Unmixer
 module ProductionBreakpoints
   module Breakpoints
     class Base
+      attr_reader :provider_name, :name
 
       def initialize(source_file, start_line, end_line, trace_id: 1)
         @injector_module = nil
@@ -15,7 +16,9 @@ module ProductionBreakpoints
         @parser = ProductionBreakpoints::Parser.new(@source_file)
         @node = @parser.find_definition_node(@start_line, @end_line)
         @ns = Object.const_get(@parser.find_definition_namespace(@node))
-        @tracepoint = StaticTracing::Tracepoint.new(File.basename(@source_file).gsub('.', '_'), "#{@method}_#{@trace_id}", Integer)
+        @provider_name = File.basename(@source_file).gsub('.', '_')
+        @name = "#{@method}_#{@trace_id}"
+        @tracepoint = StaticTracing::Tracepoint.new(@provider_name, @name, Integer)
       end
 
       def install
@@ -53,9 +56,11 @@ module ProductionBreakpoints
       # It will inject handlers before and after the target lines, allowing
       # us to keep the expected binding for the wrapped code, and remainder of the method
       def build_redefined_definition_module(node)
-        injected = @parser.inject_ruby_block("local_bind=binding; ProductionBreakpoints.installed_breakpoints[#{@trace_id}].handle(local_bind)",
-                                             "ProductionBreakpoints.installed_breakpoints[#{@trace_id}].finish(local_bind)",
+        handler = "local_bind=binding; ProductionBreakpoints.installed_breakpoints[:#{@trace_id}].handle(local_bind)"
+        finisher = "ProductionBreakpoints.installed_breakpoints[:#{@trace_id}].finish(local_bind)"
+        injected = @parser.inject_ruby_block(handler, finisher,
                                              node.first_lineno, node.last_lineno, @start_line, @end_line)
+        #ProductionBreakpoints.logger.debug(injected)
         Module.new { module_eval{ eval(injected); eval('def production_breakpoint_enabled?; true; end;') } }
       end
     end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,3 +9,10 @@ require_relative '../lib/ruby-production-breakpoints.rb'
 def ruby_source_testfile_path(source_name)
   File.join(__dir__, 'ruby_sources', source_name)
 end
+
+def find_provider_fd(provider_name)
+  Dir["/proc/#{Process.pid}/fd/*"].each do |fd|
+    return fd if File.readlink(fd).include?(provider_name)
+  rescue Errno::ENOENT
+  end
+end
