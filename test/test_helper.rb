21cd7d7a633c5e7994529f70388c71bbaf9928d4
Dale Hamel <dale.hamel@srvthe.net>
2019-07-21 22:14:50 -0400

    separated fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,11 +8,11 @@ require 'pry-byebug' if ENV['PRY']
 require_relative '../lib/ruby-production-breakpoints.rb'
 
 def ruby_source_testfile_path(source_name)
-  File.join(__dir__, 'ruby_sources', source_name)
+  File.join(__dir__, 'fixtures', 'ruby_sources', source_name)
 end
 
 def config_testfile_path(config)
-  File.join(__dir__, 'config', config)
+  File.join(__dir__, 'fixtures', 'config', config)
 end
 
 def find_provider_fd(provider_name)
